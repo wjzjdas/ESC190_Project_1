1e7df345c64377387c88677067f97984c41ffa7d
wjzjdas <eddiefan050804@gmail.com>
2024-02-14 17:32:57 -0500

    c

--- autocomplete.h
+++ autocomplete.h
@@ -7,7 +7,9 @@ typedef struct term{
 } term;
 
 
-void read_in_terms(struct term **terms, int *pnterms, char *filename);
+void read_in_terms(struct term **terms, int *pnterms, char *filename){
+    
+}
 int lowest_match(struct term *terms, int nterms, char *substr);
 int highest_match(struct term *terms, int nterms, char *substr);
 void autocomplete(struct term **answer, int *n_answer, struct term *terms, int nterms, char *substr);
