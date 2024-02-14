f1069dda983bbf6702cb0ca0f3e0d1e16f5f579a
wjzjdas <eddiefan050804@gmail.com>
2024-02-14 17:46:33 -0500

    c

--- autocomplete.h
+++ autocomplete.h
@@ -13,7 +13,9 @@ void read_in_terms(struct term **terms, int *pnterms, char *filename){
 int lowest_match(struct term *terms, int nterms, char *substr){
     
 }
-int highest_match(struct term *terms, int nterms, char *substr);
+int highest_match(struct term *terms, int nterms, char *substr){
+    
+}
 void autocomplete(struct term **answer, int *n_answer, struct term *terms, int nterms, char *substr);
 
 #endif
