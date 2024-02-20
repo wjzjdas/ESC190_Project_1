3c7efef439648a1467ce14ff8b3e20ae8f9b22e8
wjzjdas <eddiefan050804@gmail.com>
2024-02-20 17:50:51 -0500

    c

--- autocomplete.h
+++ autocomplete.h
@@ -1,6 +1,8 @@
 #if !defined(AUTOCOMPLETE_H)
 #define AUTOCOMPLETE_H
 
+#include <string.h>
+
 typedef struct term{
     char term[200]; // assume terms are not longer than 200
     double weight;
@@ -14,7 +16,37 @@ int lowest_match(struct term *terms, int nterms, char *substr){
     
 }
 int highest_match(struct term *terms, int nterms, char *substr){
+    int index = -1;
 
+    //For Binary Searching
+    int min = 0;
+    int max = nterms - 1;
+
+    // min <= max because we are finding the highest match, so we must continue the loop until compare goes from 0 to positive
+    while (min <= max){ 
+        
+        // [enf(min) eng enga(mid) engb enh enha(max)] 
+        // for even lists, We are using the element before the median as mid
+        int mid = min + (max - min) / 2; 
+
+        char temp[200];
+        strcpy(temp, terms[mid].term); 
+        int len = strlen(substr);
+        int compare = strcmp(substr, temp); // -1, 0, 1
+
+
+        if (compare <= 0){ 
+            if (strlen(temp) >= len && substr[len-1] >= temp[len-1]){
+                index = mid;
+                min = mid + 1; // so now we are left with [engb(min) enh enha(max)]
+            } else {
+                max = mid - 1;
+            }
+        } else {
+            max = mid - 1;
+        }
+    }
+    return index;
 }
 void autocomplete(struct term **answer, int *n_answer, struct term *terms, int nterms, char *substr){
 
